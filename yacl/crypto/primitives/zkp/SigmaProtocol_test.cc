2e54c7bda731031e4838f02896f5db3e9198dda1
rainbowwx <928748754@qq.com>
2023-04-11 15:45:19 +0800

    modify 2

--- yacl/crypto/primitives/zkp/SigmaProtocol_test.cc
+++ yacl/crypto/primitives/zkp/SigmaProtocol_test.cc
@@ -4,30 +4,86 @@
 
 namespace yacl::crypto::test {
 
-auto curve = openssl::OpensslGroup::Create(GetCurveMetaByName("sm2"));
-MPInt n = curve->GetOrder();
+class SigmaProtocolTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    curve = openssl::OpensslGroup::Create(GetCurveMetaByName("sm2"));
+    n = curve->GetOrder();
+    auto cofactor = curve->GetCofactor();
 
-TEST(SigmaProtocolTest, DlogTest) {
-  SigmaMeta Dlog = {SigmaType::Dlog, 1, 1, 1};
+    witness.resize(3, (MPInt)0);
+    rnd_witness.resize(3, (MPInt)0);
+    generators.reserve(3);
+    for (uint32_t i = 0; i < 3; i++) {
+      // init random witness & rnd_witness
+      MPInt::RandomLtN(n, &witness[0]);
+      MPInt::RandomLtN(n, &rnd_witness[0]);
+
+      // sample generators
+      generators.emplace_back(curve->MulBase(0_mp));
+      uint32_t j = 0;
+      EcPoint tmp = curve->MulBase(0_mp);
+      while (curve->IsInfinity(tmp)) {
+        tmp = curve->HashToCurve(HashToCurveStrategy::TryAndRehash_SHA2,
+                                 fmt::format("id{}", j++));
+        generators[i] = tmp;
+        curve->MulInplace(&tmp, cofactor);
+      }
+    }
+  }
+
+  void StartTest(SigmaMeta SigmaType, ByteContainerView other_info) {
+    SigmaProtocol protocol(curve, generators, SigmaType);
+    std::vector<EcPoint> statement = protocol.ToStatement(witness);
+
+    auto proof_batch =
+        protocol.ProveBatch(witness, statement, rnd_witness, other_info);
+
+    EXPECT_TRUE(protocol.VerifyBatch(statement, proof_batch, other_info));
+
+    auto proof_short =
+        protocol.ProveShort(witness, statement, rnd_witness, other_info);
+
+    EXPECT_TRUE(protocol.VerifyShort(statement, proof_short, other_info));
+  }
+
+  std::unique_ptr<yacl::crypto::EcGroup> curve;
+  MPInt n;
 
   std::vector<MPInt> witness;
   std::vector<MPInt> rnd_witness;
-  ByteContainerView other_info("DlogTest");
-
-  witness.emplace_back(0);
-  MPInt::RandomLtN(n, &witness[0]);
-  rnd_witness.emplace_back(0);
-  MPInt::RandomLtN(n, &rnd_witness[0]);
-
   std::vector<EcPoint> generators;
-  generators.emplace_back(curve->GetGenerator());
+};
 
-  SigmaProtocol protocol(curve, generators, Dlog);
-  std::vector<EcPoint> statement = protocol.ToStatement(witness);
+TEST_F(SigmaProtocolTest, DlogTest) {
+  SigmaMeta Dlog = {SigmaType::Dlog, 1, 1, 1};
+  ByteContainerView other_info("DlogTest");
+  StartTest(Dlog, other_info);
+}
 
-  auto proof = protocol.ProveBatch(witness, statement, rnd_witness, other_info);
+TEST_F(SigmaProtocolTest, RepresentationTest) {
+  // The Pedersen is the same as RepresentationTest
+  SigmaMeta Representation = {SigmaType::Representation, 3, 3, 1};
+  ByteContainerView other_info("RepresentationTest");
+  StartTest(Representation, other_info);
+}
 
-  EXPECT_TRUE(protocol.VerifyBatch(statement, proof, other_info));
+TEST_F(SigmaProtocolTest, SeveralDlogTest) {
+  SigmaMeta SeveralDlog = {SigmaType::SeveralDlog, 3, 3, 3};
+  ByteContainerView other_info("SeveralDlogTest");
+  StartTest(SeveralDlog, other_info);
+}
+
+TEST_F(SigmaProtocolTest, SeveralDlogEqTest) {
+  SigmaMeta SeveralDlogEq = {SigmaType::SeveralDlogEq, 1, 3, 3};
+  ByteContainerView other_info("SeveralDlogEqTest");
+  StartTest(SeveralDlogEq, other_info);
+}
+
+TEST_F(SigmaProtocolTest, DHTrippleTest) {
+  SigmaMeta DHTripple = {SigmaType::DHTripple, 1, 2, 2};
+  ByteContainerView other_info("DHTrippleTest");
+  StartTest(DHTripple, other_info);
 }
 
 }  // namespace yacl::crypto::test
